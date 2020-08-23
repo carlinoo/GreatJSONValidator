5329f3524998937a979b9b91010529af8009ced7
Carlino Gonzalez <carlinoo@icloud.com>
2020-08-24 00:37:32 +0200

    aded matches test

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ let schema = {
     properties: {
         string: { type: 'string', required: true, format: "iso8601", validate(data) { return true; } },
         string2: { type: 'string', matches: /\d-(?:days|weeks|months|years)/},
+        string3: { type: 'string', matches: /^y[1-5]m[1-9](?:[0-2])?$/ },
         number: { type: 'number', required: true, less_than: 10, greater_than: 1, equal_to: 9 },
         undefined: { type: 'undefined', required: true },
         null: { type: 'null', required: true },
@@ -40,6 +41,7 @@ let schema = {
 let data = {
     string: "2019-06-26T20:06:18.658Z",
     string2: "12323-days",
+    string3: "y1m9",
     number: 9,
     null: null,
     boolean: true,
